53248b1d6ae03b5232ef77472c3936f227b28f1e
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-09 15:23:13 +0200

    Add Bot fields

--- models/component.go
+++ models/component.go
@@ -3,15 +3,18 @@ package models
 import "time"
 
 type Component struct {
-	ID             int             `json:"id"`
-	Name           string          `json:"name"`
-	ParentID       int             `json:"parent_id"`
-	CreatedAt      time.Time       `json:"created_at" time_format:"2006-01-02 15:04:05"`
-	UpdatedAt      time.Time       `json:"updated_at" time_format:"2006-01-02 15:04:05"`
-	Description    string          `json:"description"`
-	Owner          string          `json:"owner"`
-	Status         ComponentStatus `json:"status"`
-	SlackChannelID string          `json:"slack_channel_id"`
+	ID              int             `json:"id"`
+	Name            string          `json:"name"`
+	ParentID        *int            `json:"parent_id"`
+	CreatedAt       time.Time       `json:"created_at" time_format:"2006-01-02 15:04:05"`
+	UpdatedAt       time.Time       `json:"updated_at" time_format:"2006-01-02 15:04:05"`
+	Description     string          `json:"description"`
+	Owner           string          `json:"owner"`
+	Status          ComponentStatus `json:"status"`
+	SlackChannelID  *string         `json:"slack_channel_id"`
+	IsBotEnabled    bool            `json:"is_bot_enabled"`
+	BotKnowledge    []string        `json:"bot_knowledge"`
+	BotInstructions []string        `json:"bot_instructions"`
 }
 
 type ComponentStatus string
