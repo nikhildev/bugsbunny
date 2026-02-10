41413f48315d22cab5cbf82b14f57f09da4131cd
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-10 20:07:10 +0200

    More reorganise for simpilicity

--- api/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/nikhildev/bugsbunny/api/cmd/commands"
-)
-
-func main() {
-	commands.Execute()
-}
---
+++ api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nikhildev/bugsbunny/api/commands"
+
+func main() {
+	commands.Execute()
+}
