a6565570ae2c8ee7b92dad7b5bfd50849f3d2896
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-15 16:25:23 +0200

    Adding some basic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import { Routes, Route } from "react-router";
+import { Home } from "./pages/Home";
 import IssuesList from "./pages/issues/IssuesList";
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<IssuesList />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/issues" element={<IssuesList />} />
     </Routes>
   );
 }
