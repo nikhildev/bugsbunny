782f4a16627791f90120e95e6031911355114a3d
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-15 16:56:31 +0200

    Adding a nice data table

--- frontend/src/pages/issues/IssuesList.tsx
+++ frontend/src/pages/issues/IssuesList.tsx
@@ -1,12 +1,66 @@
+import { DataTable } from "@/components/DataTable";
 import { getAllIssues } from "./api/api";
 import { useState, useEffect } from "react";
+import type { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/components/ui/badge";
 
 interface Issue {
   id: string;
   title: string;
   description: string;
   status: string;
+  createdAt: string;
+  updatedAt: string;
+  type: string;
+  reporter: string;
+  componentId: string;
+  priority: string;
+  severity: string;
 }
+
+export const columns: ColumnDef<Issue, any>[] = [
+  {
+    header: "Title",
+    accessorKey: "title",
+  },
+  {
+    header: "Created At",
+    accessorKey: "created_at",
+  },
+  {
+    header: "Updated At",
+    accessorKey: "updated_at",
+  },
+  {
+    header: "Type",
+    accessorKey: "type",
+    cell: ({ row }) => {
+      return <>{row.original.type.toUpperCase()}</>;
+    },
+  },
+  {
+    header: "Reporter",
+    accessorKey: "reporter",
+  },
+  {
+    header: "Component ID",
+    accessorKey: "component_id",
+  },
+  {
+    header: "Priority",
+    accessorKey: "priority",
+    cell: ({ row }) => {
+      return <Badge>{row.original.priority.toUpperCase()}</Badge>;
+    },
+  },
+  {
+    header: "Severity",
+    accessorKey: "severity",
+    cell: ({ row }) => {
+      return <Badge>{row.original.severity.toUpperCase()}</Badge>;
+    },
+  },
+];
 
 export const IssuesList = () => {
   const [issues, setIssues] = useState<Issue[]>([]);
@@ -14,14 +68,8 @@ export const IssuesList = () => {
     getAllIssues().then(setIssues);
   }, []);
 
-  return (
-  <div>
+  return <div>
     <h1>Issues</h1>
-    <ul>
-      {issues.map((issue) => (
-        <li key={issue.id}>{issue.title}</li>
-        ))}
-        </ul>
-    </div>
-  );
+    <DataTable columns={columns} data={issues} />
+  </div>;
 };
