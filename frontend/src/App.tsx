728f724745b47d99218fdd3b68d3d7c1c0612540
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-18 16:02:11 +0200

    Adding sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
 import "./index.css";
 import { Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
-import {IssuesList} from "./pages/issues/IssuesList";
+import { IssuesList } from "./pages/issues/IssuesList";
+import { AppSidebar } from "./components/AppSidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/issues" element={<IssuesList />} />
-    </Routes>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <SidebarTrigger />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/issues" element={<IssuesList />} />
+          </Routes>
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
 
