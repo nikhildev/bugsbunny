0c4b7b672f8df5ae58974247fdc110e1219ea990
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-18 16:13:07 +0200

    Adding items to sidebar

--- frontend/src/components/AppSidebar.tsx
+++ frontend/src/components/AppSidebar.tsx
@@ -3,16 +3,59 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Link } from "react-router";
+import { HomeIcon, ListIcon, ComponentIcon } from "lucide-react";
 
 export const AppSidebar = () => {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <h1 className="text-2xl font-semibold text-gray-700">Bugs Bunny</h1>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          {/* <SidebarGroupLabel>Pages</SidebarGroupLabel> */}
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <Link to="/" className="flex items-center gap-2">
+                    <HomeIcon className="size-4" />
+                    Home
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <Link to="/issues" className="flex items-center gap-2">
+                    <ListIcon className="size-4" />
+                    Issues List
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <Link to="/components" className="flex items-center gap-2">
+                    <ComponentIcon className="size-4" />
+                    Components
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
