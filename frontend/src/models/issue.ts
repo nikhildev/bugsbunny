8baa926a5fee0e083d0c0751c917e43a43ee85a3
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-19 16:32:34 +0200

    Defining better model for issue

--- frontend/src/models/issue.ts
+++ frontend/src/models/issue.ts
@@ -2,12 +2,34 @@ export interface Issue {
   id: string;
   title: string;
   description: string;
-  status: string;
+  status: IssueStatus;
   created_at: string;
   updated_at: string;
-  type: string;
-  reporter: string;
+  type: IssueType;
+  reporter: User;
   component_id: string;
-  priority: string;
-  severity: string;
+  priority: Priority;
+  severity: Severity;
+}
+
+export type IssueStatus =
+  | "new"
+  | "resolved"
+  | "in_progress"
+  | "reopened"
+  | "blocked"
+  | "on_hold";
+export type IssueType =
+  | "bug"
+  | "feature"
+  | "support"
+  | "improvement"
+  | "documentation";
+export type Priority = "low" | "medium" | "high" | "critical";
+export type Severity = "low" | "medium" | "high" | "critical";
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
 }
