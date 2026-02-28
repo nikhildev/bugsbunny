90d88448058f0149c705dfa6657ca796e84e7556
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2026-02-28 21:58:47 +0200

    fix: rename GetComponentByIDHandler typo fix

--- api/routes/component/routes.go
+++ api/routes/component/routes.go
@@ -5,7 +5,7 @@ import "net/http"
 func RegisterComponentRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /components", GetComponentsHandler)
 	mux.HandleFunc("POST /components", CreateComponentHandler)
-	mux.HandleFunc("GET /components/{id}", GetComponentByIHandler)
+	mux.HandleFunc("GET /components/{id}", GetComponentByIDHandler)
 	mux.HandleFunc("DELETE /components/{id}", DeleteComponentHandler)
 	mux.HandleFunc("PATCH /components/{id}", UpdateComponentHandler)
 }
